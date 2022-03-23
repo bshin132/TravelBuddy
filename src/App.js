d137628921dbcf06b12c46e647a0ef7e486efaf0
brian shin <bshin132@gmail.com>
2022-03-23 13:30:47 -0700

    build searchinput comp and install font awesome icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Button from './components/Button/Button';
-import DestinationCard from './components/DestinationCard/DestinationCard';
+import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <Button label="Generate New" type="defaultButton"/>
-      <Button label="icon" type="backButton"/>
+      <SearchBar />
     </div>
   );
 }

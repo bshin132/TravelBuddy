21e92d5584afa8889ac814680e6e64a2e05cd391
Xinyu Chen <xinyuchen98@gmail.com>
2022-03-24 21:34:33 +0000

    Use PG to setup database

---
+++ bin/resetdb.js
@@ -0,0 +1,53 @@
+// other dependencies
+import fs from "fs";
+import chalk from "chalk";
+import pg from "pg";
+import dbParams from "../lib/db.js";
+const { Client } = pg; 
+const db = new Client(dbParams);
+
+// PG connection setup
+// const connectionString = process.env.DATABASE_URL ||
+//   `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
+// const client = new Client();
+
+// Loads the schema files from db/schema
+const runSchemaFiles = async () => {
+  console.log(chalk.cyan(`-> Loading Schema Files ...`));
+  const schemaFilenames = fs.readdirSync("./db/schema");
+
+  for (const fn of schemaFilenames) {
+    const sql = fs.readFileSync(`./db/schema/${fn}`, "utf8");
+    console.log(`\t-> Running ${chalk.green(fn)}`);
+    await db.query(sql);
+  }
+};
+
+const runSeedFiles = async () => {
+  console.log(chalk.cyan(`-> Loading Seeds ...`));
+  const schemaFilenames = fs.readdirSync("./db/seeds");
+
+  for (const fn of schemaFilenames) {
+    const sql = fs.readFileSync(`./db/seeds/${fn}`, "utf8");
+    console.log(`\t-> Running ${chalk.green(fn)}`);
+    await db.query(sql);
+  }
+};
+
+const runResetDB = async () => {
+  try {
+    dbParams.host &&
+      console.log(`-> Connecting to PG on ${dbParams.host} as ${dbParams.user}...`);
+    dbParams.connectionString &&
+      console.log(`-> Connecting to PG with ${dbParams.connectionString}...`);
+    await db.connect();
+    await runSchemaFiles();
+    await runSeedFiles();
+    db.end();
+  } catch (err) {
+    console.error(chalk.red(`Failed due to error: ${err}`));
+    db.end();
+  }
+};
+
+runResetDB();
---
+++ db/seeds/01_seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO users (name) 
+VALUES ('Joe Bloggs'), 
+('John Doe'), 
+('Jane Smith');
+
+INSERT INTO provinces (full_name, short_name)
+VALUES ('Ontario', 'ON'), 
+('Quebec', 'QC'),
+('Nova Scotia', 'NS'),
+('New Brunswick', 'NB'),
+('Manitoba', 'MB'),
+('British Columbia', 'BC'),
+('Prince Edward Island', 'PE'),
+('Saskatchwan', 'SK'),
+('Alberta', 'AB'),
+('Newfoundland and Labrador', 'NL');
+
+INSERT INTO destinations (name, google_place_id)
+VALUES ('Jasper', 'ChIJKT5VaAgrg1MRgigZC70CIB8'), 
+('Banff', 'ChIJ_UoMkUXKcFMRY5Ss2r6ur8o'), 
+('Vancouver', 'ChIJs0-pQ_FzhlQRi_OBm-qWkbs'), 
+('Lake Louise', 'ChIJEc7hoChdd1MR5rxlY3s8Nzw'), 
+('Toronto', 'ChIJpTvG15DL1IkRd8S0KlBVNTI'), 
+('Montreal', 'ChIJDbdkHFQayUwR7-8fITgxTmU'), 
+('Revelstoke', 'ChIJpd1q-gU3eVMRyu_WuX1kPE8'), 
+('Glacier', 'ChIJK2ViqSkIeVMRWvnKB3j8i9w'), 
+('Haida Gwaii', 'ChIJAzMGVmJvbFQR673y7geLkuw'), 
+('Quebec City', 'ChIJk4jbBYqWuEwRAzro8GMtxY8'), 
+('Louisbourg', 'ChIJbSHDjFQdaEsR3aRei1h4IVQ');

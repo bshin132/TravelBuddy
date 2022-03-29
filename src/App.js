78b499ecd685162104874bec710afb02b2392cef
Xinyu Chen <xinyuchen98@gmail.com>
2022-03-29 17:48:03 -0600

    Added login route and favorites route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.scss";
+import Login from "./pages/Login/Login";
 import Homepage from "./pages/Homepage/Homepage";
 import Details from "./pages/Details/Details";
 import Random from "./pages/Random/Random";
+import Favorites from "./pages/Favorites/Favorites";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -10,10 +12,11 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="login/:user_id" element={<Login />}/>
           <Route index element={<Homepage />} />
           <Route path="details/:id" element={<Details />} />
           <Route path="random" element={<Random />} />
-          {/* <Route path="/favortie" element={<Favorite />} /> */}
+          <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </BrowserRouter>
     </div>

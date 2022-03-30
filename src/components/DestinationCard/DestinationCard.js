bbe9a9e221c7f7f58ff2ef045c1916707a866578
Xinyu Chen <xinyuchen98@gmail.com>
2022-03-29 19:10:03 -0600

    Change Navbar and homepage styling

--- src/components/DestinationCard/DestinationCard.js
+++ src/components/DestinationCard/DestinationCard.js
@@ -18,6 +18,7 @@ const Container = styled.div`
   box-shadow: 1px 15px 63px -16px rgba(0, 0, 0, 0.5);
   -webkit-box-shadow: 1px 15px 63px -16px rgba(0, 0, 0, 0.5);
   -moz-box-shadow: 1px 15px 63px -16px rgba(0, 0, 0, 0.5);
+  }
 `;
 
 const HeaderContainer = styled.div`
@@ -31,12 +32,16 @@ const HeaderContainer = styled.div`
 
 const Title = styled.p`
   font-weight: bold;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+  text-align: left;
   position: relative;
   margin-bottom: -7px;
 `;
 
 const Subtitle = styled.p`
   font-size: 10px;
+  text-shadow: -0.5px 0 black, 0 0.5px black, 0.5px 0 black, 0 -0.5px black;
+  text-align: left;
   padding-left: 2px;
 `;
 
